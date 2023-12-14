718e5199717f26f5a9e8da069875a326fb0e8bcc
baishali2502 <baishali2502@gmail.com>
2023-12-14 21:05:45 +0530

    Added UC-1_push_elements_and_top element

--- Stack/src/com/bridgelabz/stack/stack.java
+++ Stack/src/com/bridgelabz/stack/stack.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.stack;
 
+
+
 // @desc:Implementing stack using linked-list
 
 public class stack<T> 
@@ -10,6 +12,38 @@ public class stack<T>
 	{
 		head=null;
 		size=0;
+	}
+	
+//----------------------------------- UC-1 --------------------------------------
+	
+	/*
+	 * @desc:This method returns the top element of the stack
+	 * 
+	 * @returns:the element at top
+	 * 
+	 * @params:none
+	 */
+	public T top()
+	{
+		if(size()==0)
+			return null;
+		else
+		    return head.data;
+	}
+
+	/*
+	 * @desc:This method adds an element in the stack
+	 * 
+	 * @params:New element to be added
+	 * 
+	 * @returns:void
+	 */
+	public void push(T elem)
+	{
+		Node<T> newnode = new Node<T>(elem);
+		newnode.next = head;
+		head = newnode;
+		size++;
 	}
 
 }
